2a6fdb73e3e3394461a602f82b3f558f88209f90
Tomassavila <tomahsavila12@gmail.com>
2026-08-27 12:10:49 -0500

    use inpút

--- mi_programa.py
+++ mi_programa.py
@@ -1,9 +1,10 @@
 print("Hola mundo")
 
-mi_nombre= "Tomas"
+mi_nombre= input("escriba su nombre")
 
-print(mi_nombre)
+print("hola"+ mi_nombre)
 
 numero_1=5 
 numero_2=2
 print(numero_1 + numero_2)
+
